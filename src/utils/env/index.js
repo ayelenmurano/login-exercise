a19e6eb18527af3a032c21e77d9a9a5366c3c824
ayelenmurano <77493317+ayelenmurano@users.noreply.github.com>
2022-07-01 12:29:43 -0300

    The reading of the .env file is adjusted

--- src/utils/env/index.js
+++ src/utils/env/index.js
@@ -3,7 +3,7 @@ const tokenKey = process.env.TOKEN_KEY
 
 const port = process.env.PORT || 3000;
 
-if ( !process.env.DATABASE_URL || !(  process.env.DATABASE_USER &&  process.env.DATABASE_PASS &&  process.env.DATABASE_SCHEMA )) throw new Error (`DATABASE_URL or DATABASE_USER and DATABASE_PASS and DATABASE_SCHEMA must be configured.`);
+if ( !process.env.DATABASE_URL && !(  process.env.DATABASE_USER &&  process.env.DATABASE_PASS &&  process.env.DATABASE_SCHEMA )) throw new Error (`DATABASE_URL or DATABASE_USER and DATABASE_PASS and DATABASE_SCHEMA must be configured.`);
 const databaseUrl = process.env.DATABASE_URL 
 ? process.env.DATABASE_URL 
 : `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASS}@cluster0.s0im9.mongodb.net/${process.env.DATABASE_SCHEMA}?retryWrites=true&w=majority`;
